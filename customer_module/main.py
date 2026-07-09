1711097836650b486f3a3f79d5f9bc6c3fe84cd8
piuu09 <priyanavghade@gmail.com>
2026-07-09 17:51:08 +0530

    function_calling
    Refactored customer interaction loop to handle invalid input and exceptions.

--- customer_module/main.py
+++ customer_module/main.py
@@ -1,21 +1,41 @@
 from crud import *
 
-
 # calling
+while True:
+   try:
+      print(" 1. Add customer \n 2. Search Customer \n 3.Update Customer \n 4.Delete Customer \n 5.display_all_customer \n 6. Exit")
+      choice=int(input(" Enter your choice :"))
+      if choice==1:
+         add_customer()
 
-print(" 1. Add customer \n 2. Search Customer \n 3.Update Customer \n 4.Delete Customer \n 5.display_all_customer \n 6. Exit")
-choice=int(input(" enter your choice :"))
-if choice==1:
-   add_customer()
-elif choice==2:
-   search_customer()
-elif choice==3:
-   update_customer()
-elif choice==4:
-   delete_customer()
-elif choice==5:
-  display_all_customer()
-elif choice==6:
-   print(" Exit")        
-else:
-   print(" Invalid choice")
+      elif choice==2:
+         search_customer()
+
+      elif choice==3:
+         update_customer()
+
+      elif choice==4:
+         delete_customer()
+
+      elif choice==5:
+         display_all_customer()
+
+      elif choice==6:
+         print(" Exit")  
+         break      
+
+      else:
+         print(" Invalid choice")
+
+
+   except ValueError:
+      print(" Error: Please enter only numbers. ")
+
+   except Exception as e:
+      print(" An error occurred:",e)
+
+   finally:
+      print(" Program Ended")
+
+
+
